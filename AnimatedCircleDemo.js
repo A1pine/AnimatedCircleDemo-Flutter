280eb01fea6b96f63f2d90047c104c6a05fcd035
“Xuning <u6792826@anu.edu.au>
2020-07-06 18:25:31 +0800

    Formatted a little bit

--- AnimatedCircleDemo.js
+++ AnimatedCircleDemo.js
@@ -14,6 +14,7 @@ if (canvas.getContext) {
     console.log('浏览器不支持canvas')
 }
 var slider = document.getElementById('slide')
+
 function draw_rectangle(ctx, x, y, width, height) {
     ctx.strokeStyle = "#000"
     ctx.lineWidth = 1
@@ -54,34 +55,34 @@ function updateSlider(slideAmount) {
     var pointD = new coordinate(getDirection(progress) ? rectangle_cor.x - movedDistance * 2 : rectangle_cor.x, rectCenter_cor.y)
 
     //c1-c8 八个辅助点
-    var c1 = new coordinate(pointA.x + offset , pointA.y)
-    var c2 = new coordinate(pointB.x , getDirection(progress) ? pointB.y - offset : pointB.y - offset + movedDistance)
+    var c1 = new coordinate(pointA.x + offset, pointA.y)
+    var c2 = new coordinate(pointB.x, getDirection(progress) ? pointB.y - offset : pointB.y - offset + movedDistance)
 
-    var c3 = new coordinate(pointB.x , getDirection(progress) ? pointB.y + offset : pointB.y + offset - movedDistance)
-    var c4 = new coordinate(pointC.x + offset , pointC.y)
+    var c3 = new coordinate(pointB.x, getDirection(progress) ? pointB.y + offset : pointB.y + offset - movedDistance)
+    var c4 = new coordinate(pointC.x + offset, pointC.y)
 
-    var c5 = new coordinate(pointC.x - offset , pointC.y)
-    var c6 = new coordinate(pointD.x , getDirection(progress) ? pointD.y + offset - movedDistance : pointD.y + offset)
+    var c5 = new coordinate(pointC.x - offset, pointC.y)
+    var c6 = new coordinate(pointD.x, getDirection(progress) ? pointD.y + offset - movedDistance : pointD.y + offset)
 
-    var c7 = new coordinate(pointD.x , getDirection(progress) ? pointD.y - offset + movedDistance : pointD.y - offset)
+    var c7 = new coordinate(pointD.x, getDirection(progress) ? pointD.y - offset + movedDistance : pointD.y - offset)
     var c8 = new coordinate(pointA.x - offset, pointA.y)
 
     //绘制贝塞尔曲线
     //左上部分
     ctx.moveTo(pointD.x, pointD.y)
-    ctx.bezierCurveTo(c7.x,c7.y,c8.x,c8.y,pointA.x,pointA.y)
+    ctx.bezierCurveTo(c7.x, c7.y, c8.x, c8.y, pointA.x, pointA.y)
 
     //右上部分
     ctx.moveTo(pointA.x, pointA.y)
-    ctx.bezierCurveTo(c1.x,c1.y,c2.x,c2.y,pointB.x,pointB.y)
+    ctx.bezierCurveTo(c1.x, c1.y, c2.x, c2.y, pointB.x, pointB.y)
 
     //右下部分
-    ctx.moveTo(pointB.x,pointB.y)
-    ctx.bezierCurveTo(c3.x,c3.y,c4.x,c4.y,pointC.x,pointC.y)
+    ctx.moveTo(pointB.x, pointB.y)
+    ctx.bezierCurveTo(c3.x, c3.y, c4.x, c4.y, pointC.x, pointC.y)
 
     //左下部分
-    ctx.moveTo(pointC.x,pointC.y)
-    ctx.bezierCurveTo(c5.x,c5.y,c6.x,c6.y,pointD.x,pointD.y)
+    ctx.moveTo(pointC.x, pointC.y)
+    ctx.bezierCurveTo(c5.x, c5.y, c6.x, c6.y, pointD.x, pointD.y)
     ctx.fillStyle = 'red'
     ctx.stroke()
 
@@ -109,19 +110,21 @@ function updateSlider(slideAmount) {
 
 
 }
-function getDirection(progress){
-    if(progress <= 0.5){
+
+function getDirection(progress) {
+    if (progress <= 0.5) {
         console.log("B点动");
         return false;
-    }
-    else{
+    } else {
         console.log("D点动")
         return true;
     }
 }
+
 function getRads(degrees) {
     return (Math.PI * degrees) / 180;
 }
+
 function getDegrees(rads) {
     return (rads * 180) / Math.PI;
 }
